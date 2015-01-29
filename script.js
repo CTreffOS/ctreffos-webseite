5df730edfe815bf32473ac3f6e00f5ee3ed34ba9
Dr. Peter Voigt <pvoigt@uos.de>
2015-01-29 18:32:31 +0100

    Added debug code to test why February meetings are not calculated

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
+//
+// JavaScript debug messages can be viewed in the error console of
+// Firefox >= 20.0.x. Shortcut to open error console is
+// Ctrl+Shfit+J or F12.
+//
+// TO BE DONE:
+// Currently algorithm fails for meetings in February for unknown
+// reason.
+// 
+
+
 function getMondaysOfMonth(month, year) {
-    var currentDate = new Date(),
-        mondays = [];
+    var currentDate = new Date();
+    var mondays = [];
+    console.log('Inside function getMondaysOfMonth.');
     currentDate.setMonth(month);
     currentDate.setFullYear(year);
     // Start at the first day of a month.
     currentDate.setDate(1);
+    console.log('First day of month that is checked for next meeting:');
+    console.log(currentDate);
     // Get the first Monday in the month.
     while (currentDate.getDay() !== 1) {
         currentDate.setDate(currentDate.getDate() + 1);
@@ -19,65 +33,97 @@ function getMondaysOfMonth(month, year) {
         mondays.push(new Date(currentDate.getTime()));
         currentDate.setDate(currentDate.getDate() + 7);
     }
+    console.log('Leaving function getMondaysOfMonth.');
     return mondays;
 }
+
 
 function toRFC2822String(dateString) {
     // Convert date to RFC2822 conform string, like:
     // Mon, 22 Sep 2014 19:00:00 +0200
     var timezone;
-    dateString = ('' + dateString).split(' ');
-    timezone = dateString[5].split('T')[1];
-    rfc2822String = dateString[0]
-                    + ', ' + dateString[2]
-                    + ' ' + dateString[1]
-                    + ' ' + dateString[3]
-                    + ' ' + dateString[4]
-                    + ' ' + timezone;
+    console.log('Inside function toRFC2822String');
+    if (dateString === undefined) {
+        rfc2822String = 'ERROR: Cannot convert date.';
+    }
+    else {
+        dateString = ('' + dateString).split(' ');
+        timezone = dateString[5].split('T')[1];
+        rfc2822String = dateString[0]
+                        + ', ' + dateString[2]
+                        + ' ' + dateString[1]
+                        + ' ' + dateString[3]
+                        + ' ' + dateString[4]
+                        + ' ' + timezone;
+    }
+    console.log('Leaving function toRFC2822String');
     return rfc2822String;
 }
+
 
 function toISO8601String(dateString) {
     // Convert date to ISO8601 conform string in local timezone, like:
     // 2014-09-22T19:00:00+0200
-    var timezone,
-        months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul",
-                  "Aug", "Sep", "Oct", "Nov", "Dec"],
-        month;
-    dateString = ('' + dateString).split(' ');
-    month = '' + (months.indexOf(dateString[1]) + 1);
-    if (month.length == 1) {
-        month = "0" + month;
+    var timezone;
+    var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul",
+                  "Aug", "Sep", "Oct", "Nov", "Dec"];
+    var month;
+    console.log('Inside function toISO8601String.');
+    if (dateString === undefined) {
+        iso8601String = 'ERROR: Cannot convert date.';
     }
-    timezone = dateString[5].split('T')[1];
-    iso8601String = dateString[3]
-                    + '-' + month
-                    + '-' + dateString[2]
-                    + 'T' + dateString[4]
-                    + timezone;
+    else {
+        dateString = ('' + dateString).split(' ');
+        month = '' + (months.indexOf(dateString[1]) + 1);
+        if (month.length == 1) {
+            month = "0" + month;
+        }
+        timezone = dateString[5].split('T')[1];
+        iso8601String = dateString[3]
+                        + '-' + month
+                        + '-' + dateString[2]
+                        + 'T' + dateString[4]
+                        + timezone;
+    }
+    console.log('Leaving function toISO8601String.');
     return iso8601String;
 }
 
+
 function getNextMeeting() {
-    var nextMeeting,
-        currentDate,
-        nextMeetingMonth,
-        nextMeetingYear;
-    currentDate = new Date();
+    var nextMeeting;
+    var currentDate = new Date();
+    var nextMeetingMonth;
+    var nextMeetingYear;
     // Test with a fixed date.
-    // currentDate = new Date(2015, 0, 26, 19, 01, 00, 00);
+    currentDate = new Date(2015, 0, 19, 19, 01, 00, 00);
+    console.log('Inside function getNextMeeting.');
+    console.log('Current date:');
+    console.log(currentDate);
     nextMeetingMonth = currentDate.getMonth();
     nextMeetingYear = currentDate.getFullYear();
     nextMeeting = getMondaysOfMonth(nextMeetingMonth, nextMeetingYear)[3];
+    console.log('Next meeting found.');
+    console.log(nextMeeting);
+    console.log('Checking, if found next meeting of this month is already over.');
+    console.log(nextMeeting.getTime());
+    console.log(currentDate.getTime());
     if (nextMeeting.getTime() < currentDate.getTime()) {
+        console.log('Meeting this month is over.');
         if (currentDate.getMonth() < 11) {
+            console.log('Current month < 11.');
             nextMeetingMonth = currentDate.getMonth() + 1;
             nextMeeting = getMondaysOfMonth(nextMeetingMonth, nextMeetingYear)[3];
+            console.log('Next meeting of next month.');
+            console.log(nextMeeting);
         }
-        else {
+        else if (currentDate.getMonth() === 11) {
+            console.log('Current month = 11');
             nextMeetingMonth = 0;
             nextMeetingYear = currentDate.getFullYear() + 1;
             nextMeeting = getMondaysOfMonth(nextMeetingMonth, nextMeetingYear)[3];
+            console.log('Next meeting of first month of next year.');
+            console.log(nextMeeting);
         }
     }
     // Convert to string, like:
@@ -88,10 +134,16 @@ function getNextMeeting() {
     // ISO8601 string in UTC timezone, like:
     // 2014-09-22T17:00:00.000Z
     // nextMeeting = nextMeeting.toISOString();
+    console.log('Final value of next meeting:');
+    console.log(nextMeeting);
+    console.log('Leaving function getNextMeeting.');
     return nextMeeting;
 }
 
+
 function setMeeting() {
+    console.log('Inside function setMeeting.');
     document.getElementById('nextMeeting').innerHTML = getNextMeeting();
+    console.log('Leaving function setMeeting.');
 }
 
